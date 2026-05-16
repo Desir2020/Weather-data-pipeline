f00987000b24575213161fad79cf5c9cc4cfe359
Marc Désir Bouanga Niambi <>
2026-05-16 17:03:15 +0200

    feat: create weather table schema

---
+++ sql/create_tables.sql
@@ -0,0 +1,21 @@
+CREATE TABLE weather (
+    id SERIAL PRIMARY KEY,
+    city VARCHAR(150) NOT NULL,
+    country VARCHAR(10),
+
+    latitude DOUBLE PRECISION,
+    longitude DOUBLE PRECISION,
+    recorded_at TIMESTAMP NOT NULL,
+
+    weather_main VARCHAR(50),
+    weather_description TEXT,
+
+    temperature DOUBLE PRECISION NOT NULL,
+    feels_like DOUBLE PRECISION,
+    temp_min DOUBLE PRECISION,
+    temp_max DOUBLE PRECISION,
+
+    humidity INT,
+    pressure INT,
+    wind_speed DOUBLE PRECISION
+);
